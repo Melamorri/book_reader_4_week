be0f8fb784ecb37385d2174262af89b8093c3010
Melamorri <105131998+Melamorri@users.noreply.github.com>
2022-08-18 01:14:39 +0300

    ридер

---
+++ android/app/src/main/kotlin/com/example/book_reader_4_week/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.book_reader_4_week
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int fontSize = 14;
+
+  void minusSize() {
+    if (fontSize > 0) {
+      setState(() {
+        fontSize--;
+      });
+    }
+  }
+
+  void plusSize() {
+    if (fontSize < 42) {
+      setState(() {
+        fontSize++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Читалка",
+            style: TextStyle(color: Color.fromARGB(255, 255, 196, 0)),
+          ),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 254, 255, 171),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  backgroundColor: Colors.amber,
+                  mini: true,
+                  tooltip: 'Уменьшить',
+                  onPressed: () {
+                    minusSize();
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+                Container(
+                  width: 140,
+                  height: 40,
+                  margin: EdgeInsets.only(top: 10),
+                  child: TextField(
+                    controller: TextEditingController()
+                      ..text = fontSize.toString(),
+                    enableInteractiveSelection: false,
+                    decoration: InputDecoration(
+                        labelText: "Размер шрифта",
+                        labelStyle: TextStyle(
+                            color: Color.fromARGB(255, 253, 190, 0),
+                            fontSize: 15),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1, color: Colors.amber),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              width: 2,
+                              color: Color.fromARGB(255, 248, 223, 112)),
+                          borderRadius: BorderRadius.circular(15),
+                        )),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ], // Only numbers can be entered
+                    onSubmitted: (String font) {
+                      int newFont = int.parse(font);
+                      if (newFont > 0 && newFont < 43) {
+                        setState(() {
+                          fontSize = newFont;
+                        });
+                      }
+                    },
+                  ),
+                ),
+                FloatingActionButton(
+                  backgroundColor: Colors.amber,
+                  mini: true,
+                  tooltip: 'Увеличить',
+                  onPressed: () {
+                    plusSize();
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(
+                height: 450,
+                padding: EdgeInsets.all(20),
+                child: Text(
+                    'Винни-пух и пятачок отдыхали на веточке дуба. Пух сказал: ”Интересно, как долго мы ещё будем здесь торчать?” Пятачок ответил: ” Я думаю, ещё лет десять."',
+                    style: TextStyle(
+                        fontSize: fontSize.toDouble(),
+                        fontWeight: FontWeight.normal)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
